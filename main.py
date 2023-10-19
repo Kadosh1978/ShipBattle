e9864955c4968615a12425454e95a738c0d9a1fd
Kadosh1978 <147097651+Kadosh1978@users.noreply.github.com>
2023-10-19 20:39:59 +0300

    ShipBattle

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import random
 
 def create_random_ship():
     return random.randint(0, 5), random.randint(0, 5)
-# проверка 2
-#
+
 def play_again():
     try_again = input("Wanna play again? <Y>es or <N>o? >: ").lower()
     if try_again == "y":
